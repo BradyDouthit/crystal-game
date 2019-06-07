23c82264c4fee8b1c67b4cc586592c0a8f158eb3
Brady Douthit <brady.douthit99@gmail.com>
2019-06-07 19:38:35 -0400

    JS is done(probably). Styling has begun.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,10 @@
+//gets a random number between 120 and 19.
 var getRandomNumber = function() {
     var x = document.getElementById("number-box")
     x.innerHTML = Math.floor((Math.random() * 120) + 19);
     return x.innerHTML;
   }
+//variables go here
 var randomNumber = getRandomNumber();
 var numberArray = Array();
 var addedNumberValue = 0;
@@ -11,6 +13,7 @@ var losses = 0;
 var lossesElement = document.getElementById("loss-count");
 var wins = 0;
 var winsElement = document.getElementById("win-count");
+//gets a random number between 12 and 1, then pushes the value to numberArray, 4 times
 var randomCrystalValue = function() {
   for (var i = 0; i < 4; i++) {
   var selectedNumber = Math.floor((Math.random() * 12) + 1)
@@ -25,14 +28,14 @@ randomCrystalValue();
 
 console.log("addedNumberValue = " + addedNumberValue); 
 console.log("randomNumber = " + randomNumber); 
-
+//on click events for the buttons
 $("#crystal1").on("click", function() {
   var numberValue = numberArray[0];
   addedNumberValue = addedNumberValue + numberValue;
   addedNumberElement.innerHTML = addedNumberValue;
   if (addedNumberValue > randomNumber){
     losses = losses + 1;
-    lossesElement.innerHTML = losses + " times";
+    lossesElement.innerHTML = losses + " time(s)";
     numberArray = [];
     randomCrystalValue();
     randomNumber = getRandomNumber();
@@ -41,7 +44,7 @@ $("#crystal1").on("click", function() {
   }
   if (addedNumberValue == randomNumber) {
     wins = wins + 1;
-    winsElement.innerHTML = wins + " times";
+    winsElement.innerHTML = wins + " time(s)";
     numberArray = [];
     randomCrystalValue();
     randomNumber = getRandomNumber();
@@ -55,7 +58,7 @@ $("#crystal2").on("click", function() {
   addedNumberElement.innerHTML = addedNumberValue;
   if (addedNumberValue > randomNumber){
     losses = losses + 1;
-    lossesElement.innerHTML = losses + " times";
+    lossesElement.innerHTML = losses + " time(s)";
     numberArray = [];
     randomCrystalValue();
     randomNumber = getRandomNumber();
@@ -64,7 +67,7 @@ $("#crystal2").on("click", function() {
   }
   if (addedNumberValue == randomNumber) {
     wins = wins + 1;
-    winsElement.innerHTML = wins + " times";
+    winsElement.innerHTML = wins + " time(s)";
     numberArray = [];
     randomCrystalValue();
     randomNumber = getRandomNumber();
@@ -78,7 +81,7 @@ $("#crystal3").on("click", function() {
   addedNumberElement.innerHTML = addedNumberValue;
   if (addedNumberValue > randomNumber){
     losses = losses + 1;
-    lossesElement.innerHTML = losses + " times";
+    lossesElement.innerHTML = losses + " time(s)";
     numberArray = [];
     randomCrystalValue();
     randomNumber = getRandomNumber();
@@ -87,7 +90,7 @@ $("#crystal3").on("click", function() {
   }
   if (addedNumberValue == randomNumber) {
     wins = wins + 1;
-    winsElement.innerHTML = wins + " times";
+    winsElement.innerHTML = wins + " time(s)";
     numberArray = [];
     randomCrystalValue();
     randomNumber = getRandomNumber();
@@ -101,7 +104,7 @@ $("#crystal4").on("click", function() {
   addedNumberElement.innerHTML = addedNumberValue;
   if (addedNumberValue > randomNumber){
     losses = losses + 1;
-    lossesElement.innerHTML = losses + " times";
+    lossesElement.innerHTML = losses + " time(s)";
     numberArray = [];
     randomCrystalValue();
     randomNumber = getRandomNumber();
@@ -110,7 +113,7 @@ $("#crystal4").on("click", function() {
   }
   if (addedNumberValue == randomNumber) {
     wins = wins + 1;
-    winsElement.innerHTML = wins + " times";
+    winsElement.innerHTML = wins + " time(s)";
     numberArray = [];
     randomCrystalValue();
     randomNumber = getRandomNumber();
